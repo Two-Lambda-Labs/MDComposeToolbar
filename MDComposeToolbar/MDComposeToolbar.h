a836537366c2796122c1142a5c332c49abeb79e8
Jonathan Danek <jonathan@danek.me>
2019-01-30 01:55:52 -0500

    Initial Commit

---
+++ MDComposeToolbar/MDComposeToolbar.h
@@ -0,0 +1,19 @@
+//
+//  MDComposeToolbar.h
+//  MDComposeToolbar
+//
+//  Created by Jonathan Danek on 1/30/19.
+//  Copyright © 2019 Two Lambda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDComposeToolbar.
+FOUNDATION_EXPORT double MDComposeToolbarVersionNumber;
+
+//! Project version string for MDComposeToolbar.
+FOUNDATION_EXPORT const unsigned char MDComposeToolbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDComposeToolbar/PublicHeader.h>
+
+
